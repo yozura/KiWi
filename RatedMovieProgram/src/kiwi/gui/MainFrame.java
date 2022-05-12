20eb62abe555ecc65b2e81440bac9ee79a942345
yozura <yozura43@naver.com>
2022-05-13 00:05:38 +0900

    init all

---
+++ RatedMovieProgram/src/kiwi/gui/MainFrame.java
@@ -0,0 +1,14 @@
+package kiwi.gui;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+	public MainFrame() {
+		this.setTitle("KiWi");
+		
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(1280, 800);
+		this.setVisible(true);
+	}
+}
---
+++ RatedMovieProgram/src/kiwi/main/Main.java
@@ -0,0 +1,9 @@
+package kiwi.main;
+
+import kiwi.gui.*;
+
+public class Main {
+	public static void main(String[] args) {
+		new MainFrame();
+	}
+}
